e43657495474b135818173f28d92beb5a1d8cadd
Christoph33one <cjbrowning23@hotmail.com>
2023-04-01 16:40:50 +0000

    added a create function to LikeSerializer to handle user liking post more than once

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Like
 
@@ -11,6 +12,17 @@ class LikeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Like
+        # stops the user from liking the same post twice
         fields = [
             'id', 'created_at', 'owner', 'post'
         ]
+
+    # this stops the server crashing and gives a IntegrityError
+    # 'detail': 'possible duplicate' - user is liking the same post twice!
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
