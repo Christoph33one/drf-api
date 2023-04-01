42bd070fb25afa9f51f548d8b5511e792331ef38
Christoph33one <cjbrowning23@hotmail.com>
2023-04-01 17:16:23 +0000

    created LikeList and LikeDetail to control like function and delete a like

--- likes/serializers.py
+++ likes/serializers.py
@@ -17,12 +17,14 @@ class LikeSerializer(serializers.ModelSerializer):
             'id', 'created_at', 'owner', 'post'
         ]
 
-    # this stops the server crashing and gives a IntegrityError
-    # 'detail': 'possible duplicate' - user is liking the same post twice!
+    # how to prvent out API users from liking a post twice!
     def create(self, validated_data):
+        # this stops the server crashing and gives a IntegrityError
+        # 'detail': 'possible duplicate' - user is liking the same post twice!
         try:
             return super().create(validated_data)
         except IntegrityError:
             raise serializers.ValidationError({
                 'detail': 'possible duplicate'
             })
+
