4db45b99c91dcbeaf6a322a856007e304fd14bfd
Christoph33one <cjbrowning23@hotmail.com>
2023-04-11 12:38:11 +0000

    updated settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -65,10 +65,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [
-    '... .herokuapp.com',
-    'localhost',
-]
+ALLOWED_HOSTS = ['localhost', 'django-rest-project.herokuapp.com']
 
 # Application definition
 
